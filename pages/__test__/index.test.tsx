179777cb4f327b6b3af6ff6df8b5d7d462b150f9
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 14:22:40 +0700

    add jest

---
+++ pages/__test__/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Home from "../index";
+
+describe("Home", () => {
+  it("renders a home page", () => {
+    const { container } = render(<Home />);
+    const heading = screen.getByText("Hello world!");
+    expect(heading).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+});
