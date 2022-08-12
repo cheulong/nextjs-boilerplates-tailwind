ade2b775634a8552e8074fcca305dde4082e2adb
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 14:29:00 +0700

    add prettier

--- pages/__test__/index.test.tsx
+++ pages/__test__/index.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../index";
+import { render, screen } from '@testing-library/react';
+import Home from '../index';
 
-describe("Home", () => {
-  it("renders a home page", () => {
+describe('Home', () => {
+  it('renders a home page', () => {
     const { container } = render(<Home />);
-    const heading = screen.getByText("Hello world!");
+    const heading = screen.getByText('Hello world!');
     expect(heading).toBeInTheDocument();
     expect(container).toMatchSnapshot();
   });
