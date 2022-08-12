1d12061ff629e6add885afe18f7c207a6a0c64ff
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 21:39:22 +0700

    add translate

--- components/__tests__/index.test.tsx
+++ components/__tests__/index.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/pages/index';
+import I18nProvider from 'next-translate/I18nProvider';
+import commonEN from '../../locales/en/common.json';
 
 describe('Home', () => {
   it('renders a home page', () => {
-    const { container } = render(<Home />);
+    const { container } = render(
+      <I18nProvider lang="en" namespaces={{ common: commonEN }}>
+        <Home />
+      </I18nProvider>,
+    );
     const heading = screen.getByText('Hello world!');
     expect(heading).toBeInTheDocument();
     expect(container).toMatchSnapshot();
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,26 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import Navbar from '@/components/Navbar';
 import BaseButton from '@/components/Button/BaseButton';
+import useTranslation from 'next-translate/useTranslation';
+import setLanguage from 'next-translate/setLanguage';
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <Navbar />
+      <button onClick={async () => await setLanguage('ja')}>
+        {t('common:Japanese')}
+      </button>
+      <br />
+      <button onClick={async () => await setLanguage('en')}>
+        {t('common:English')}
+      </button>
+      <br />
+      <button onClick={async () => await setLanguage('ko')}>
+        {t('common:Korean')}
+      </button>
       <h1 className={styles.title}>Hello world!</h1>
       <BaseButton />
     </div>
