94ea5e92465f9a6ffa241707902a18764b619d88
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 21:19:59 +0700

    fix storybook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // mode: 'jit',
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
