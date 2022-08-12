92fd50e9bffb4a3e21aba1904d68710f3434fb5b
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 16:38:53 +0700

    add lintstagedrc

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+  '*.{js,jsx,ts,tsx}': ['yarn format:fix', buildEslintCommand],
+  '*.{css,scss,less}': ['yarn format:fix'],
+  '**/*.(md|json)': (filenames) => `yarn format:fix ${filenames.join(' ')}`,
+};
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 
 const Navbar: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Navba!</h1>;
+  return <h1 className="text-3xl font-bold underline">Navbar!</h1>;
 };
 
 export default Navbar;
