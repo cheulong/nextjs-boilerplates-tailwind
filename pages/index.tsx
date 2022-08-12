5629a79d5813d7f765dd373958a2623f02a03b8e
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 15:53:30 +0700

    fix absolute path

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar';
 
 const Home: NextPage = () => {
-  // return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-  return <h1 className={styles.title}>Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <h1 className={styles.title}>Hello world!</h1>
+    </div>
+  );
 };
 
 export default Home;
