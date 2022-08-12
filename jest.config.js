b5cefbd523f0b5e2abed94e28633c2c014c634d6
Cheulong Sear <cheulong.sear@7peakssoftware.com>
2022-08-12 19:29:56 +0700

    add pre-push husky

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 const nextJest = require('next/jest');
+// jest.config.js
+const { pathsToModuleNameMapper } = require('ts-jest');
+// In the following statement, replace `./tsconfig` with the path to your `tsconfig` file
+// which contains the path mapping (ie the `compilerOptions.paths` option):
+const { compilerOptions } = require('./tsconfig');
 
 const createJestConfig = nextJest({
   dir: './',
@@ -8,6 +13,10 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  modulePathIgnorePatterns: ['<rootDir>/e2e/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
 };
 
 module.exports = createJestConfig(customJestConfig);
